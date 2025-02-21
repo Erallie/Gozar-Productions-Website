c9980faa2aeee025b53279ae60099e6dc34f37b4
Erallie <31375589+Erallie@users.noreply.github.com>
2025-02-21 16:15:57 -0700

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,104 @@
+<script lang="ts">
+    import Button from "./Button.svelte";
+    import Counter from "./Counter.svelte";
+    import welcome from "$lib/images/svelte-welcome.webp";
+    import welcomeFallback from "$lib/images/svelte-welcome.png";
+</script>
+
+<svelte:head>
+    <title>Gozar Productions</title>
+    <meta
+        name="description"
+        content="Gozar Productions is a small film company on YouTube creating short films and music videos with a passion for music. Birthed from a young girl's aspirations and pet projects, this small company endeavors to share our passions with the world for all to enjoy."
+    />
+    <meta
+        property="og:title"
+        content="Gozar Productions: A Musical Film Company"
+    />
+    <meta
+        property="og:description"
+        content="Gozar Productions is a small film company on YouTube creating short films and music videos with a passion for music. Birthed from a young girl's aspirations and pet projects, this small company endeavors to share our passions with the world for all to enjoy."
+    />
+    <meta
+        property="og:image"
+        content="https://static.wixstatic.com/media/3d52c1_ea36ec3c8e3b48948b1a1d72011d99b4%7Emv2.jpeg/v1/fit/w_2500,h_1330,al_c/3d52c1_ea36ec3c8e3b48948b1a1d72011d99b4%7Emv2.jpeg"
+    />
+    <meta property="og:image:width" content="2500" />
+    <meta property="og:image:height" content="1330" />
+    <meta property="og:url" content="https://www.gozarproductions.com" />
+    <meta property="og:site_name" content="Gozar Productions" />
+    <meta property="og:type" content="website" />
+</svelte:head>
+
+<section class="page-header">
+    <hgroup class="page-header">
+        <span>Welcome to</span>
+        <h1>Gozar Productions</h1>
+        <h2>Where passion takes flight</h2>
+    </hgroup>
+
+    <!-- <Counter /> -->
+</section>
+
+<section id="about">
+    <div>
+        <h2>Who we are</h2>
+        <p>
+            Gozar Productions is a small film company on YouTube creating short
+            films and music videos with a passion for music. Birthed from a
+            young girl's aspirations and pet projects, this small company
+            endeavors to share our passions with the world for all to enjoy.
+        </p>
+    </div>
+</section>
+
+<section id="watch">
+    <div>
+        <h2>Watch us on YouTube</h2>
+        <p>
+            We believe in keeping our content as accessible as possible, even if
+            it means less money for us. The best way to do that is through
+            YouTube, where you can find all the videos we've made throughout the
+            years.
+        </p>
+        <Button>Watch us now</Button>
+    </div>
+</section>
+
+<section id="music">
+    <div>
+        <h2>Our music</h2>
+        <p>
+            The genius behind our original music is the mind of a young girl
+            named Erika Gozar. She writes all of our original scores with simply
+            an ear for music and no formal training in composition.
+        </p>
+        <Button>Listen now</Button>
+    </div>
+</section>
+
+<section id="code">
+    <div>
+        <h2>Our code</h2>
+        <p>
+            Along with our musical and cinematic endeavors, we also create a
+            number of code-based tools, ranging from Discord bots to VR aids. We
+            aim to make our projects free for anyone to use.
+        </p>
+        <Button>View our projects</Button>
+    </div>
+</section>
+
+<section id="contact">
+    <h2>Contact us</h2>
+</section>
+
+<style>
+    section {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        flex: 0.6;
+    }
+</style>
---
+++ src/routes/Button.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import logo from "$lib/images/button-arrow.svg";
+    let { children } = $props();
+</script>
+
+<button>{@render children()}<img src={logo} alt="→" /></button>
+
+<style>
+    button {
+        font-family: Montserrat;
+        --text-color: 0, 0, 0;
+        border-radius: 50px;
+        padding: 16px 28px;
+        border: 2px solid rgb(var(--text-color));
+        background-color: rgba(var(--text-color), 0);
+        text-align: center;
+        letter-spacing: 0.25em;
+        text-transform: uppercase;
+        font-size: 15px;
+        margin: 20px;
+        transition: background-color 0.3s;
+    }
+    button > img {
+        aspect-ratio: 200 / 60;
+        height: 0.8em;
+        vertical-align: middle;
+        transform: translate(0, -1px);
+        padding-left: 0.5em;
+        transition: height 0.3s;
+    }
+
+    button:hover {
+        background-color: rgba(var(--text-color), 0.15);
+    }
+    button:hover > img {
+        height: 1em;
+    }
+</style>
