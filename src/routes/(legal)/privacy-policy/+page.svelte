9399e2f238983256919d2351c1725a95e3fe892b
Erallie <31375589+Erallie@users.noreply.github.com>
2025-02-23 15:37:32 -0700

    Added privacy policy

---
+++ src/routes/(legal)/privacy-policy/+page.svelte
@@ -0,0 +1,303 @@
+<h1>Privacy policy</h1>
+<p>
+    At Gozar Productions, accessible from <a href="/">GozarProductions.com</a>,
+    one of our main priorities is the privacy of our visitors. This Privacy
+    Policy document contains types of information that is collected and recorded
+    by Gozar Productions and how we use it.
+</p>
+<p>
+    If you have additional questions or require more information about our
+    Privacy Policy, do not hesitate to contact us.
+</p>
+<p>
+    This Privacy Policy applies only to our online activities and is valid for
+    visitors to our website with regards to the information that they shared
+    and/or collect in Gozar Productions. This policy is not applicable to any
+    information collected offline or via channels other than this website.
+</p>
+
+<h2>CONSENT</h2>
+<p>
+    By using our website, you hereby consent to our Privacy Policy and agree to
+    its terms.
+</p>
+
+<h2>INFORMATION WE COLLECT</h2>
+<p>
+    We collect personal information that you voluntarily provide to us when
+    expressing an interest in obtaining information about us or our products and
+    services, when participating in activities on the Services or otherwise
+    contacting us.
+</p>
+
+<p>
+    The personal information that we collect depends on the context of your
+    interactions with us and the Services, the choices you make and the products
+    and features you use. The personal information we collect can include the
+    following:
+</p>
+<ul>
+    <li>
+        <strong>Publicly Available Personal Information.</strong> We collect phone
+        numbers; email addresses; first name and last name; current mailing and billing
+        addresses; and other similar data.
+    </li>
+
+    <li>
+        <strong>Personal Information Provided by You.</strong> We collect financial
+        information (credit card number, purchase history, invoices); and other similar
+        data.
+    </li>
+
+    <li>
+        <strong>Payment Data.</strong> We collect data necessary to process your
+        payment if you make purchases, such as your payment instrument number
+        (such as a credit card number), and the security code associated with
+        your payment instrument. All payment data is stored by PayPal. You may
+        find their privacy policy
+        <a
+            href="https://www.paypal.com/webapps/mpp/ua/privacy-full"
+            target="_blank">here</a
+        >.
+    </li>
+
+    <li>
+        <strong>Information Automatically Collected.</strong> We automatically collect
+        certain information when you visit, use or navigate the Services. This information
+        does not reveal your specific identity (like your name or contact information)
+        but may include device and usage information, such as your IP address, browser
+        and device characteristics, operating system, language preferences, referring
+        URLs, device name, country, location, information about how and when you
+        use our Services and other technical information. This information is primarily
+        needed to maintain the security and operation of our Services, and for our
+        internal analytics and reporting purposes. Like many businesses, we also
+        collect information through cookies and similar technologies.
+    </li>
+</ul>
+<p>
+    All personal information that you provide to us must be true, complete and
+    accurate, and you must notify us of any changes to such personal
+    information.
+</p>
+
+<h2>HOW WE USE YOUR INFORMATION</h2>
+<p>
+    We use personal information collected via our Services for a variety of
+    business purposes described below. We process your personal information for
+    these purposes in reliance on our legitimate business interests, in order to
+    enter into or perform a contract with you, with your consent, and/or for
+    compliance with our legal obligations. We indicate the specific processing
+    grounds we rely on next to each purpose listed below.
+</p>
+
+<p>We use the information we collect or receive:</p>
+
+<ul>
+    <li>
+        <strong>To fulfill and manage your payments.</strong> We may use your information
+        to fulfill and manage your orders, payments, returns, and exchanges made
+        through the Services.
+    </li>
+
+    <li>
+        <strong>To respond to user inquiries/offer support to users.</strong> We
+        may use your information to respond to your inquiries and solve any potential
+        issues you might have with the use of our Services.
+    </li>
+
+    <li>
+        <strong>To send you marketing and promotional communications.</strong> We
+        may process the personal information you send to us for our marketing purposes,
+        if this is in accordance with your marketing preferences. You can opt out
+        of our marketing emails at any time by clicking on the unsubscribe link in
+        the emails that we send or by contacting us directly.
+    </li>
+
+    <li>
+        <strong>For other Business Purposes.</strong> We may use your information
+        for other Business Purposes, such as data analysis, identifying usage trends,
+        determining the effectiveness of our promotional campaigns and to evaluate
+        and improve our Services, products, marketing and your experience. We may
+        use and store this information in aggregated and anonymized form so that
+        it is not associated with individual end users and does not include personal
+        information. We will not use identifiable personal information without your
+        consent.
+    </li>
+</ul>
+
+<h2>WILL YOUR INFORMATION BE SHARED WITH ANYONE?</h2>
+<p>We may process or share data based on the following legal basis:</p>
+<ul>
+    <li>
+        <strong>Consent:</strong> We may process your data if you have given us specific
+        consent to use your personal information in a specific purpose.
+    </li>
+    <li>
+        <strong>Legitimate Interests:</strong> We may process your data when it is
+        reasonably necessary to achieve our legitimate business interests.
+    </li>
+    <li>
+        <strong>Performance of a Contract:</strong> Where we have entered into a
+        contract with you, we may process your personal information to fulfill the
+        terms of our contract.
+    </li>
+    <li>
+        <strong>Legal Obligations:</strong> We may disclose your information where
+        we are legally required to do so in order to comply with applicable law,
+        governmental requests, a judicial proceeding, court order, or legal process,
+        such as in response to a court order or a subpoena (including in response
+        to public authorities to meet national security or law enforcement requirements).
+    </li>
+    <li>
+        <strong>Vital Interests:</strong> We may disclose your information where
+        we believe it is necessary to investigate, prevent, or take action regarding
+        potential violations of our policies, suspected fraud, situations involving
+        potential threats to the safety of any person and illegal activities, or
+        as evidence in litigation in which we are involved.
+    </li>
+</ul>
+
+<p>
+    More specifically, we may need to process your data or share your personal
+    information in the following situations:
+</p>
+<ul>
+    <li>
+        <strong>Business Transfers.</strong> We may share or transfer your information
+        in connection with, or during negotiations of, any merger, sale of company
+        assets, financing, or acquisition of all or a portion of our business to
+        another company.
+    </li>
+</ul>
+<h2>LOG FILES</h2>
+<p>
+    Gozar Productions follows a standard procedure of using log files. These
+    files log visitors when they visit websites. All hosting companies do this
+    and a part of hosting services' analytics. The information collected by log
+    files include internet protocol (IP) addresses, browser type, Internet
+    Service Provider (ISP), date and time stamp, referring/exit pages, and
+    possibly the number of clicks. These are not linked to any information that
+    is personally identifiable. The purpose of the information is for analyzing
+    trends, administering the site, tracking users' movement on the website, and
+    gathering demographic information.
+</p>
+<h2>COOKIES AND WEB BEACONS</h2>
+<p>
+    Like any other website, Gozar Productions uses 'cookies'. These cookies are
+    used to store information including visitors' preferences, and the pages on
+    the website that the visitor accessed or visited. The information is used to
+    optimize the users' experience by customizing our web page content based on
+    visitors' browser type and/or other information.
+</p>
+<p>
+    You can choose to disable cookies through your individual browser options.
+    To know more detailed information about cookie management with specific web
+    browsers, it can be found at the browsers' respective websites.
+</p>
+<p>
+    We also use social media buttons and/or plugins on this site that allow you
+    to connect with your social networks in various ways. For these to work,
+    these social media sites will set cookies through our site which may be used
+    to enhance your profile on their site or contribute to the data they hold
+    for various purposes outlined in their respective privacy policies. <strong
+        >THESE COOKIES MUST BE DISABLED WITHIN YOUR BROWSER SETTINGS SEPARATELY
+        FROM OUR WEBSITE. DECLINING OUR COOKIES IN THE POP-UP DOES NOT DISABLE
+        THEM.</strong
+    > You can find the privacy policies of the social media plugins we use in our
+    website here: Facebook's Privacy Policy.
+</p>
+<h2>THIRD PARTY PRIVACY POLICIES</h2>
+<p>
+    Gozar Productions's Privacy Policy does not apply to other advertisers or
+    websites. Thus, we are advising you to consult the respective Privacy
+    Policies of these third-party servers for more detailed information. It may
+    include their practices and instructions about how to opt-out of certain
+    options. View these Privacy Policies here: Wix's Privacy Policy and Google's
+    Privacy Policy.
+</p>
+<h2>CCPA PRIVACY RIGHTS</h2>
+<p>
+    Under the CCPA, among other rights, California consumers have the right to:
+</p>
+<ul>
+    <li>
+        Request that a business that collects a consumer's personal data
+        disclose the categories and specific pieces of personal data that a
+        business has collected about consumers.
+    </li>
+
+    <li>
+        Request that a business delete any personal data about the consumer that
+        a business has collected.
+    </li>
+
+    <li>
+        Request that a business that sells a consumer's personal data, not sell
+        the consumer's personal data.
+    </li>
+</ul>
+
+<p>
+    If you make a request, we have one month to respond to you. If you would
+    like to exercise any of these rights, please contact us.
+</p>
+
+<h2>GDPR DATA PROTECTION RIGHTS</h2>
+<p>
+    We would like to make sure you are fully aware of all of your data
+    protection rights. Every user is entitled to the following:
+</p>
+
+<ul>
+    <li>
+        <strong>The right to access:</strong> You have the right to request copies
+        of your personal data. We may charge you a small fee for this service.
+    </li>
+
+    <li>
+        <strong>The right to rectification:</strong> You have the right to request
+        that we correct any information you believe is inaccurate. You also have
+        the right to request that we complete the information you believe is incomplete.
+    </li>
+
+    <li>
+        <strong>The right to erasure:</strong> You have the right to request that
+        we erase your personal data, under certain conditions.
+    </li>
+
+    <li>
+        <strong>The right to restrict processing:</strong> You have the right to
+        request that we restrict the processing of your personal data, under certain
+        conditions.
+    </li>
+
+    <li>
+        <strong>The right to object to processing:</strong> You have the right to
+        object to our processing of your personal data, under certain conditions.
+    </li>
+
+    <li>
+        <strong>The right to data portability:</strong> You have the right to request
+        that we transfer the data that we have collected to another organization,
+        or directly to you, under certain conditions.
+    </li>
+</ul>
+<p>
+    If you make a request, we have one month to respond to you. If you would
+    like to exercise any of these rights, please contact us.
+</p>
+
+<h2>CHILDREN'S INFORMATION</h2>
+<p>
+    Another part of our priority is adding protection for children while using
+    the internet. We encourage parents and guardians to observe, participate in,
+    and/or monitor and guide their online activity.
+</p>
+
+<p>
+    Gozar Productions does not knowingly collect any Personal Identifiable
+    Information from children under the age of 13. If you think that your child
+    provided this kind of information on our website, we strongly encourage you
+    to contact us immediately and we will do our best efforts to promptly remove
+    such information from our records.
+</p>
