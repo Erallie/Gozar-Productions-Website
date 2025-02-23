8765a28f400defe8ad5284feed095ac2fecd9edc
Erallie <31375589+Erallie@users.noreply.github.com>
2025-02-23 14:52:24 -0700

    Added a footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import Header from "./Header.svelte";
+
+    import logo from "$lib/images/gozar-productions-logo.svg";
     import "../app.css";
 
     let { children } = $props();
@@ -17,10 +19,22 @@
     </main>
 
     <footer>
-        <p>
-            visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to
-            learn about SvelteKit
-        </p>
+        <h1>
+            <img src={logo} alt="Gozar Productions Logo" />Gozar Productions
+        </h1>
+        <div class="links">
+            <a href="/privacy-policy">Privacy Policy</a>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="100"
+                height="100"
+                viewBox="0 0 100 100"
+            >
+                <circle cx="50" cy="50" r="10" fill="currentColor" />
+            </svg>
+            <a href="/disclaimer">Disclaimer</a>
+        </div>
+        <p>© 2025 Gozar Productions LLC. All Rights Reserved.</p>
     </footer>
 </div>
 
@@ -47,15 +61,40 @@
         justify-content: center;
         align-items: center;
         padding: 12px;
+        background-color: black;
     }
 
+    footer img {
+        aspect-ratio: 1 / 1;
+        height: 4rem;
+        display: block;
+        margin: 12px auto;
+    }
+
+    footer,
+    footer a,
+    footer p {
+        color: white;
+        font-weight: 300;
+    }
+
+    footer h1 {
+        font-size: 1.5rem;
+        font-weight: 300;
+    }
+    footer svg {
+        aspect-ratio: 1 / 1;
+        width: auto;
+        height: 1em;
+        vertical-align: middle;
+    }
+
+    footer p,
     footer a {
-        font-weight: bold;
+        font-size: 0.85rem;
+        text-decoration: none;
     }
-
-    @media (min-width: 480px) {
-        footer {
-            padding: 12px 0;
-        }
+    footer a:hover {
+        text-decoration: underline;
     }
 </style>
