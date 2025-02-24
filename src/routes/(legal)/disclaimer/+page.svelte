7085b7d0c70952c8230cd53c39d5ae24568ca322
Erallie <erika@gozarproductions.com>
2025-02-24 09:34:44 -0700

    Added disclaimer page

---
+++ src/routes/(legal)/disclaimer/+page.svelte
@@ -0,0 +1,73 @@
+<script>
+    import LegalSection from "../LegalSection.svelte";
+</script>
+
+<svelte:head>
+    <title>Disclaimer</title>
+</svelte:head>
+<LegalSection>
+    <p>
+        If you require any more information or have any questions about our
+        site's disclaimer, please feel free to contact us by email at <a
+            href="mailto:contact@gozarproductions.com"
+            >contact@gozarproductions.com</a
+        >.
+    </p></LegalSection
+>
+<LegalSection id="disclaimers">
+    <h2>Disclaimers</h2>
+    <p>
+        All the information on this website - <a href="/"
+            >GozarProductions.com</a
+        > - is published in good faith and for general information purpose only.
+        Gozar Productions does not make any warranties about the completeness, reliability
+        and accuracy of this information. Any action you take upon the information
+        you find on this website (Gozar Productions), is strictly at your own risk.
+        Gozar Productions will not be liable for any losses and/or damages in connection
+        with the use of our website.
+    </p>
+
+    <p>
+        From our website, you can visit other websites by following hyperlinks
+        to such external sites. While we strive to provide only quality links to
+        useful and ethical websites, we have no control over the content and
+        nature of these sites. These links to other websites do not imply a
+        recommendation for all the content found on these sites. Site owners and
+        content may change without notice and may occur before we have the
+        opportunity to remove a link which may have gone 'bad'.
+    </p>
+
+    <p>
+        Please be also aware that when you leave our website, other sites may
+        have different privacy policies and terms which are beyond our control.
+        Please be sure to check the Privacy Policies of these sites as well as
+        their "Terms of Service" before engaging in any business or uploading
+        any information.
+    </p></LegalSection
+>
+
+<LegalSection id="consent"
+    ><h2>Consent</h2>
+    <p>
+        By using our website, you hereby consent to our disclaimer and agree to
+        its terms.
+    </p></LegalSection
+>
+
+<LegalSection id="update"
+    ><h2>Update</h2>
+    <p>
+        Should we update, amend or make any changes to this document, those
+        changes will be prominently posted here.
+    </p>
+</LegalSection>
+
+<style>
+    a {
+        color: rgb(151, 151, 255);
+        text-decoration: none;
+    }
+    a:hover {
+        text-decoration: underline;
+    }
+</style>
