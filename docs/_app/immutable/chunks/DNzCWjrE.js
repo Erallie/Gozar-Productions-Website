78c912bb9212af0875c497cf1e70ae46fc1a804b
Erallie <erika@gozarproductions.com>
2025-03-04 12:59:12 -0700

    Built github pages

---
+++ docs/_app/immutable/chunks/DNzCWjrE.js
@@ -0,0 +1,1 @@
+import{o as a}from"./Dpa4uJhH.js";a();
---
+++ docs/_app/immutable/nodes/4.Dj92vJ82.js
@@ -0,0 +1,19 @@
+import{t as a,h as p,a as t}from"../chunks/D7gTxMWC.js";import"../chunks/DNzCWjrE.js";import{f,$ as y,s as i,a0 as s}from"../chunks/Dpa4uJhH.js";import{L as n}from"../chunks/9BV7kLF6.js";var v=a(`<p>If you require any more information or have any questions about our
+		site's disclaimer, please feel free to contact us by email at <a href="mailto:contact@gozarproductions.com" class="svelte-1j61v21">contact@gozarproductions.com</a>.</p>`),b=a(`<h2>Disclaimers</h2> <p>All the information on this website - <a href="/" class="svelte-1j61v21">GozarProductions.com</a> - is published in good faith and for general information purpose only.
+		Gozar Productions does not make any warranties about the completeness, reliability
+		and accuracy of this information. Any action you take upon the information
+		you find on this website (Gozar Productions), is strictly at your own risk.
+		Gozar Productions will not be liable for any losses and/or damages in connection
+		with the use of our website.</p> <p>From our website, you can visit other websites by following hyperlinks
+		to such external sites. While we strive to provide only quality links to
+		useful and ethical websites, we have no control over the content and
+		nature of these sites. These links to other websites do not imply a
+		recommendation for all the content found on these sites. Site owners and
+		content may change without notice and may occur before we have the
+		opportunity to remove a link which may have gone 'bad'.</p> <p>Please be also aware that when you leave our website, other sites may
+		have different privacy policies and terms which are beyond our control.
+		Please be sure to check the Privacy Policies of these sites as well as
+		their "Terms of Service" before engaging in any business or uploading
+		any information.</p>`,1),w=a(`<h2>Consent</h2> <p>By using our website, you hereby consent to our disclaimer and agree to
+		its terms.</p>`,1),g=a(`<h2>Update</h2> <p>Should we update, amend or make any changes to this document, those
+		changes will be prominently posted here.</p>`,1),P=a("<!> <!> <!> <!>",1);function G(m){var l=P();p(e=>{y.title="Disclaimer"});var c=f(l);n(c,{children:(e,r)=>{var o=v();t(e,o)}});var h=i(c,2);n(h,{id:"disclaimers",children:(e,r)=>{var o=b();s(6),t(e,o)}});var d=i(h,2);n(d,{id:"consent",children:(e,r)=>{var o=w();s(2),t(e,o)}});var u=i(d,2);n(u,{id:"update",children:(e,r)=>{var o=g();s(2),t(e,o)}}),t(m,l)}export{G as component};
