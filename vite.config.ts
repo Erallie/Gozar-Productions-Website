ad3e630ba04d47e4d112ba306ef43ae1c35d9e3e
Erallie <31375589+Erallie@users.noreply.github.com>
2026-07-04 13:05:07 -0700

    Revert "Added robots.txt and sitemap"
    This reverts commit 636646aa86d5fe50771895db3be8f48a616efa5a.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { sitemap } from 'vite-plugin-sitemap';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		sitemap({
-			hostname: 'https://gozarproductions.com'
-		})
-	]
+	plugins: [sveltekit()]
 });
