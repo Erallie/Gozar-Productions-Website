636646aa86d5fe50771895db3be8f48a616efa5a
Erallie <31375589+Erallie@users.noreply.github.com>
2026-07-04 12:50:28 -0700

    Added robots.txt and sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sitemap } from 'vite-plugin-sitemap';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		sitemap({
+			hostname: 'https://gozarproductions.com'
+		})
+	]
 });
