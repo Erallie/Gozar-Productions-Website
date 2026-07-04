089ce20b9b18a26e29d15f06a1da221903b42c9f
Erallie <31375589+Erallie@users.noreply.github.com>
2026-07-04 12:52:24 -0700

    fixed import issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { sitemap } from 'vite-plugin-sitemap';
+import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
 	plugins: [
