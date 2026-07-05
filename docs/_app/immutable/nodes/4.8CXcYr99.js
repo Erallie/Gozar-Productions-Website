695ba1dcf06941499fcf2a9b51269972667a85b1
Erallie <31375589+Erallie@users.noreply.github.com>
2026-07-04 21:51:59 -0700

    rebuilt website

---
+++ docs/_app/immutable/nodes/4.8CXcYr99.js
@@ -0,0 +1,15 @@
+import{a as o,f as a}from"../chunks/CcSTs0_j.js";import{h as u}from"../chunks/CwUnjlb7.js";import{n as h,x as g,v as s,$ as v,a0 as i}from"../chunks/D3IJNndM.js";import{L as n}from"../chunks/B9FmKru7.js";var y=a('<link rel="canonical" href="https://gozarproductions.com/disclaimer"/>'),$=a(`<p>All information on this website - <a href="/" class="svelte-in4xf">GozarProductions.com</a> -
+		and its subdomains is provided in good faith for general informational purposes
+		only. While we strive to offer accurate and reliable content, Gozar Productions
+		makes no warranties regarding the completeness, reliability, or accuracy
+		of the information. Any actions you take based on the information found on
+		this website are strictly at your own risk. Gozar Productions will not be
+		liable for any losses or damages arising from the use of our website.</p> <p>Our website may contain links to external sites. While we aim to provide
+		high-quality links to useful and ethical websites, we do not control the
+		content or nature of these external sites. These links do not imply
+		endorsement of all content found on these sites. Site owners and content
+		may change without notice, and we cannot be held responsible for any
+		issues arising from external links.</p>`,1),b=a(`<p>By using our website, you consent to this disclaimer and agree to its
+		terms.</p>`),w=a(`<p>Any updates or changes to this disclaimer will be posted here
+		prominently.</p> <p><strong>Last Updated:</strong> March 9th, 2025</p>`,1),_=a(`<p>If you have any questions or concerns about this disclaimer, please feel
+		free to contact us at:</p> <p><strong>Email:</strong> <a href="mailto:contact@gozarproductions.com" class="svelte-in4xf">contact@gozarproductions.com</a></p>`,1),k=a("<!> <!> <!> <!>",1);function G(m){var l=k();u("in4xf",t=>{var r=y();g(()=>{v.title="Disclaimer"}),o(t,r)});var c=h(l);n(c,{title:"Disclaimers",children:(t,r)=>{var e=$();i(2),o(t,e)},$$slots:{default:!0}});var d=s(c,2);n(d,{title:"Consent",children:(t,r)=>{var e=b();o(t,e)},$$slots:{default:!0}});var f=s(d,2);n(f,{title:"Update",children:(t,r)=>{var e=w();i(2),o(t,e)},$$slots:{default:!0}});var p=s(f,2);n(p,{title:"Contact",children:(t,r)=>{var e=_();i(2),o(t,e)},$$slots:{default:!0}}),o(m,l)}export{G as component};
