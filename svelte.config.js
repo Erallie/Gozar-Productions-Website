ac04d941c7f9330ec0b2ceaed121f4ae71a65d5d
Erallie <31375589+Erallie@users.noreply.github.com>
2026-07-04 22:50:53 -0700

    trying to fix website rendering blank

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import adapter from "@sveltejs/adapter-static";
 
+const dev = process.argv.includes("dev");
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
